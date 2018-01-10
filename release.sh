d454934a4056397a2ee91c51460e3b06193f89ee
Rich Alimi <ralimi@google.com>
2018-01-09 22:51:29 -0800

    Add support for travis-ci to build releases, as well as a script to tag code for which a release should be built

---
+++ release.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -eu
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Usage:
+#  (1) Update version in manifest.json
+#  (2) Run release.sh
+
+cd $(dirname $0)
+
+function die() {
+  echo $1
+  exit 1
+}
+
+# Read current version from manifest.
+readonly MANIFEST=${PWD}/manifest.json
+readonly VERSION=$(cat "${MANIFEST}" | python -c "import sys, json; print json.load(sys.stdin)['version']")
+readonly TAG=v${VERSION}
+
+# Ensure the tag doesn't already exist.  This could happen if someone forgot to
+# update the version in manifest.json.
+test -z $(git tag | grep --line-regexp "${TAG}") \
+  || die "Version ${VERSION} already exists"
+
+# Update all vendor packages and ensure everything still builds
+# and tests pass.
+govendor fetch +vendor
+make
+
+# Commit everything. This should include the change to manifest.json and any
+# updated vendored packages.
+git add .
+git commit -m "Bump to version ${VERSION}"
+
+# Tag the release.
+git tag -a "${TAG}" -m "Tag version ${VERSION}"
+
+# Push to remote repository, both changes and tags.
+git push
+git push --tags
