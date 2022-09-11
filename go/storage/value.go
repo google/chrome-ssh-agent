02895edef862da8dd70f1b54d5999971a8fe11da
Rich Alimi <ralimi@google.com>
2022-09-10 21:35:05 -0700

    Add library to store a singular typed value in storage

---
+++ go/storage/value.go
@@ -0,0 +1,79 @@
+//go:build js
+
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"syscall/js"
+	"errors"
+
+	"github.com/google/chrome-ssh-agent/go/jsutil"
+	"github.com/norunners/vert"
+)
+
+const (
+	// valueKey is the key under which the singular value is stored.
+	valueKey = "current"
+)
+
+var (
+	errParse = errors.New("parse failed")
+)
+
+// Value reads and writes a singular value.
+type Value[V any] struct {
+	store Area
+}
+
+// NewValue returns a new Value using the underlying persistent store.
+// keyPrefix is the prefix used to distinguish values from others in the same
+// underlying store; multiple may be supplied to support migration scenarios.
+func NewValue[V any](store Area, keyPrefix []string) *Value[V] {
+	return &Value[V]{
+		store: NewView(keyPrefix, store),
+	}
+}
+
+// Get reads the current value from storage.  If it doesn't exist, the zero
+// value is returned.
+func (v *Value[V]) Get(ctx jsutil.AsyncContext) (V, error) {
+	var zero V
+
+	data, err := v.store.Get(ctx)
+	if err != nil {
+		return zero, err
+	}
+
+	val, present := data[valueKey]
+	if !present {
+		return zero, nil
+	}
+
+	var tv V
+	if err := vert.ValueOf(val).AssignTo(&tv); err != nil {
+		return zero, errParse
+	}
+
+	return tv, nil
+}
+
+// Set writes a new value to storage.
+func (v *Value[V]) Set(ctx jsutil.AsyncContext, val V) error {
+	data := map[string]js.Value{
+		valueKey: vert.ValueOf(val).JSValue(),
+	}
+	return v.store.Set(ctx, data)
+}
---
+++ go/storage/value_test.go
@@ -0,0 +1,125 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"syscall/js"
+	"testing"
+
+	"github.com/google/chrome-ssh-agent/go/jsutil"
+	jut "github.com/google/chrome-ssh-agent/go/jsutil/testing"
+	st "github.com/google/chrome-ssh-agent/go/storage/testing"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/norunners/vert"
+)
+
+func TestValueGet(t *testing.T) {
+	testcases := []struct {
+		description string
+		init        map[string]js.Value
+		want        myStruct
+		wantErr     error
+	}{
+		{
+			description: "no value present",
+			want: myStruct{},
+		},
+		{
+			description: "parse values",
+			init: map[string]js.Value{
+				testKeyPrefix + "." + valueKey: vert.ValueOf(&myStruct{IntField: 42}).JSValue(),
+			},
+			want: myStruct{IntField: 42},
+		},
+		{
+			description: "error on unparseable value",
+			init: map[string]js.Value{
+				testKeyPrefix + "." + valueKey: js.ValueOf(42),
+			},
+			wantErr: errParse,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.description, func(t *testing.T) {
+			jut.DoSync(func(ctx jsutil.AsyncContext) {
+				store := NewRaw(st.NewMemArea())
+				if err := store.Set(ctx, tc.init); err != nil {
+					t.Fatalf("Set failed: %v", err)
+				}
+
+				vs := NewValue[myStruct](store, testKeyPrefixes)
+
+				got, err := vs.Get(ctx)
+				if diff := cmp.Diff(got, tc.want); diff != "" {
+					t.Errorf("incorrect result: -got +want: %s", diff)
+				}
+				if diff := cmp.Diff(err, tc.wantErr, cmpopts.EquateErrors()); diff != "" {
+					t.Errorf("incorrect error: -got +want: %s", diff)
+				}
+			})
+		})
+	}
+}
+
+func TestValueSet(t *testing.T) {
+	testcases := []struct {
+		description string
+		init        map[string]js.Value
+		write       myStruct
+		want        myStruct
+	}{
+		{
+			description: "write initial value",
+			write: myStruct{IntField: 100},
+			want:myStruct{IntField: 100},
+		},
+		{
+			description: "overwrite previous value",
+			init: map[string]js.Value{
+				testKeyPrefix + "." + valueKey: vert.ValueOf(&myStruct{StringField: "foo"}).JSValue(),
+			},
+			write: myStruct{IntField: 42},
+			want: myStruct{IntField: 42},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.description, func(t *testing.T) {
+			jut.DoSync(func(ctx jsutil.AsyncContext) {
+				store := NewRaw(st.NewMemArea())
+				if err := store.Set(ctx, tc.init); err != nil {
+					t.Fatalf("Set failed: %v", err)
+				}
+
+				vs := NewValue[myStruct](store, testKeyPrefixes)
+
+				if err := vs.Set(ctx, tc.write); err != nil {
+					t.Fatalf("Value.Set failed: %v", err)
+				}
+
+				got, err := vs.Get(ctx)
+				if err != nil {
+					t.Fatalf("Value.Get failed: %v", err)
+				}
+				if diff := cmp.Diff(got, tc.want); diff != "" {
+					t.Errorf("incorrect result: -got +want: %s", diff)
+				}
+			})
+		})
+	}
+}
+
