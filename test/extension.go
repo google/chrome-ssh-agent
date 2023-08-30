a379d975f9e94cdffcb612730aebb2d1bb88021e
Rich Alimi <ralimi@google.com>
2023-08-29 20:50:11 -0700

    Create beta build (#156)
    * Create beta build for separate publishing to webstore.
    
    * Add workflow to publish beta release
    
    * Validate that prod and beta manifest versions match
    
    * Update Go dependencies (#155)

--- test/extension.go
+++ test/extension.go
@@ -4,11 +4,7 @@ import (
 	"net/url"
 )
 
-const (
-	extensionId = "eechpbnaifiimgajnomdipfaamobdfha"
-)
-
-func makeExtensionUrl(relPath string, queryString string) *url.URL {
+func makeExtensionUrl(extensionId string, relPath string, queryString string) *url.URL {
 	return &url.URL{
 		Scheme:   "chrome-extension",
 		Host:     extensionId,
