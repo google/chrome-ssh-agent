d683432a86a854f8a43438c5f8bc599bdfacec86
Rich Alimi <ralimi@google.com>
2024-12-23 21:04:31 -0800

    Ensure pnpm runs in workspace root directory. (#370)
    * Ensure pnpm runs in workspace root directory.
    
    * Refresh dependencies

--- .vscode/refresh-dependencies.sh
+++ .vscode/refresh-dependencies.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)/..
 
 # NPM dependencies
-bazel run -- @pnpm install
+bazel run -- @pnpm install --dir="$(pwd)"
 
 # Incorporate new dependencies in go.mod
 bazel run @rules_go//go -- mod tidy
